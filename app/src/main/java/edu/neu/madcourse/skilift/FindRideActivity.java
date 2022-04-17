aa5f8c59fa9eedf91ee46678f888b52adebffaa2
Dylan Horgan <dyhorgan@gmail.com>
2022-04-17 02:50:10 -0400

    done for the night

--- app/src/main/java/edu/neu/madcourse/skilift/FindRideActivity.java
+++ app/src/main/java/edu/neu/madcourse/skilift/FindRideActivity.java
@@ -1,14 +1,30 @@
 package edu.neu.madcourse.skilift;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.GoogleMap;
 
 import android.os.Bundle;
 
 public class FindRideActivity extends AppCompatActivity {
 
-    @Override
+
+
+  @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_ride);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.maps);
+
+        //mapFragment.getMapAsync();
     }
+
+
+    public void onMapReady(@NonNull GoogleMap googleMap){
+        System.out.println("blah");
+    }
+
 }
