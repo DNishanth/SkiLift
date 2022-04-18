b727fa41dff71e902615148d8723469bd92045a2
salehi-s <salehi.s@husky.neu.edu>
2022-04-18 17:25:57 -0400

    Add Back and Submit buttons to Give a Ride activity

--- app/src/main/java/edu/neu/madcourse/skilift/GiveRideActivity.java
+++ app/src/main/java/edu/neu/madcourse/skilift/GiveRideActivity.java
@@ -2,7 +2,9 @@ package edu.neu.madcourse.skilift;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class GiveRideActivity extends AppCompatActivity {
 
@@ -10,5 +12,22 @@ public class GiveRideActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_give_ride);
+    }
+
+    protected void openActivity(Class activity) {
+        Intent intent = new Intent(this, activity);
+        startActivity(intent);
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.giveARideBackButton:
+                openActivity(MainActivity.class);
+                break;
+            case R.id.giveARideSubmitButton:
+                break;
+            default:
+                break;
+        }
     }
 }
