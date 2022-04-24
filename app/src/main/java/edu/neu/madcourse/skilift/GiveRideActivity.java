21657c023b6516aa31309c732d3a6b1e9cba288a
salehi-s <salehi.s@husky.neu.edu>
2022-04-23 23:02:06 -0400

    Remove unused imports from Give a Ride activity

--- app/src/main/java/edu/neu/madcourse/skilift/GiveRideActivity.java
+++ app/src/main/java/edu/neu/madcourse/skilift/GiveRideActivity.java
@@ -18,7 +18,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
