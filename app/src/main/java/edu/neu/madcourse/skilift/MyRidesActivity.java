f693ffa0692db646bd5cd71e0c6bde0e067500c6
Dylan Horgan <dyhorgan@gmail.com>
2022-04-25 17:20:47 -0400

    basically data transfer working

--- app/src/main/java/edu/neu/madcourse/skilift/MyRidesActivity.java
+++ app/src/main/java/edu/neu/madcourse/skilift/MyRidesActivity.java
@@ -1,14 +1,68 @@
 package edu.neu.madcourse.skilift;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.security.cert.PolicyNode;
+import java.util.ArrayList;
+
 public class MyRidesActivity extends AppCompatActivity {
+
+     ArrayList<myRideModel> rides = new ArrayList<>();
+     myRidesRecyclerViewAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_rides);
+
+      RecyclerView recyclerView = findViewById(R.id.myRidesRecycler);
+
+      populateRides();
+
+      adapter = new myRidesRecyclerViewAdapter(this, rides);
+
+
+      recyclerView.setAdapter(adapter);
+
+      recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+    }
+
+    private void populateRides(){
+      DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("rides");
+      reference.addValueEventListener(new ValueEventListener() {
+        @Override
+        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+          rides.clear();
+
+          for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+             String departureLocation = snapshot.child("departureLocation").getValue().toString();
+             String destination = snapshot.child("destination").getValue().toString();
+             String pickupDate = snapshot.child("pickupDate").getValue().toString();
+             String returnDate = snapshot.child("returnDate").getValue().toString();
+             String licensePlate = snapshot.child("licensePlate").getValue().toString();
+
+             myRideModel ride = new myRideModel(departureLocation, destination, pickupDate, returnDate, licensePlate);
+             rides.add(ride);
+          }
+          adapter.notifyDataSetChanged();
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError error) {
+
+        }
+      });
     }
 }
---
+++ app/src/main/java/edu/neu/madcourse/skilift/myRideModel.java
@@ -0,0 +1,33 @@
+package edu.neu.madcourse.skilift;
+
+public class myRideModel {
+  String origin, destination, pickup, returnTime, license;
+
+  public myRideModel(String origin, String destination, String pickup, String returnTime, String license) {
+    this.origin = origin;
+    this.destination = destination;
+    this.pickup = pickup;
+    this.returnTime = returnTime;
+    this.license = license;
+  }
+
+  public String getOrigin() {
+    return origin;
+  }
+
+  public String getDestination() {
+    return destination;
+  }
+
+  public String getPickup() {
+    return pickup;
+  }
+
+  public String getReturnTime() {
+    return returnTime;
+  }
+
+  public String getLicense() {
+    return license;
+  }
+}
