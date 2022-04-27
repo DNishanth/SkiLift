b60ca426885a075c728a3346bf34368cbc97a9ea
Dylan Horgan <dyhorgan@gmail.com>
2022-04-26 23:02:28 -0400

    basically working

--- app/src/main/java/edu/neu/madcourse/skilift/MyRidesActivity.java
+++ app/src/main/java/edu/neu/madcourse/skilift/MyRidesActivity.java
@@ -5,7 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -21,17 +26,43 @@ public class MyRidesActivity extends AppCompatActivity {
      ArrayList<myRideModel> rides = new ArrayList<>();
      myRidesRecyclerViewAdapter adapter;
      String username;
+     Button past;
+     Button upcoming;
+     Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_rides);
+        context = this;
 
       username = getIntent().getExtras().getString("username");
 
+      past = findViewById(R.id.past);
+      upcoming = findViewById(R.id.upcoming);
+      upcoming.setSelected(true);
+      populateRides(R.id.upcoming);
+
+      past.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+          past.setSelected(true);
+          upcoming.setSelected(false);
+          populateRides(R.id.past);
+        }
+      });
+      upcoming.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+          past.setSelected(false);
+          upcoming.setSelected(true);
+          populateRides(R.id.upcoming);
+        }
+      });
+
       RecyclerView recyclerView = findViewById(R.id.myRidesRecycler);
 
-      populateRides();
+
 
       adapter = new myRidesRecyclerViewAdapter(this, rides);
 
@@ -41,7 +72,8 @@ public class MyRidesActivity extends AppCompatActivity {
 
     }
 
-    private void populateRides(){
+    private void populateRides(int id){
+
       DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("rides");
       reference.addValueEventListener(new ValueEventListener() {
         @Override
@@ -56,10 +88,21 @@ public class MyRidesActivity extends AppCompatActivity {
               String pickupDate = snapshot.child("pickupDate").getValue().toString();
               String returnDate = snapshot.child("returnDate").getValue().toString();
               String licensePlate = snapshot.child("licensePlate").getValue().toString();
+              Long rideTime = Long.parseLong(snapshot.child("pickupDate").getValue().toString());
 
               myRideModel ride = new myRideModel(departureLocation, destination, pickupDate, returnDate, licensePlate);
-              rides.add(ride);
+              System.out.println("NOW:");
+              System.out.println(System.currentTimeMillis());
+              System.out.println("Ride time:");
+              System.out.println(rideTime);
+              if(id == R.id.upcoming && (rideTime > System.currentTimeMillis()) ){
+                rides.add(ride);
+              }else if(id == R.id.past && (rideTime < System.currentTimeMillis()) ){
+                rides.add(ride);
+              }
+
             }
+
           }
           adapter.notifyDataSetChanged();
         }
